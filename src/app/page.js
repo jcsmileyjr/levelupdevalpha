293f3cbc6bd7a9e3198d2cf347ebc9a8b33ea06c
JC <jcsmileyjr@gmail.com>
2024-02-17 12:35:08 -0600

    Feature: Setup the landing page header and sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 
+import Timeline from "@/components/Timeline/timeline"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Testing
+    <main className="flex min-h-screen flex-col p-4">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold underline">Level Up Dev</h1>
+        <p className="text-base mt-2">Visually track your technical skills and career progression</p>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-3/4">
+          <h2 className="text-center text-2xl">Celebrate the Journey</h2>
+          <Timeline />
+        </div>
+        <div className="w-1/4">
+          <h2 className="text-center text-xl">Passion &  Sacrifice</h2>
+        </div>
+      </div>
     </main>
   )
 }
---
+++ src/components/Timeline/timeline.js
@@ -0,0 +1,15 @@
+
+const Timeline = () => {
+    return (
+        <section>
+            <p className="text-center text-xl font-bold">Timeline</p>
+            <div className="flex flex-row">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+        </section>
+    )
+}
+
+export default Timeline;
