5f31d109ef6b812cd00fb5892bcbc7c0cff2bbdc
JC <jcsmileyjr@gmail.com>
2024-02-17 16:46:09 -0600

    Style: Landing Page design responsiveness
    Ensured that the two sections stack when in mobile view

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,13 @@ export default function Home() {
         <h1 className="text-5xl font-bold underline">Level Up Dev</h1>
         <p className="text-base mt-2">Visually track your technical skills and career progression</p>
       </div>
-      <div className="flex flex-row">
-        <div className="w-2/3">
+      <div className="flex flex-col md:flex-row ">
+        <div className="w-full order-last md:order-first md:w-2/3">
           <h2 className="text-center text-2xl">Celebrate the Journey</h2>
           <Timeline data={AcheivementTestData} />
         </div>
-        <div className="w-1/3">
-          <h2 className="text-center text-xl">Passion &  Sacrifice</h2>
+        <div className="w-full md:w-1/3 mt-6 md:mt-0">
+          <h2 className="text-center text-2xl md:text-xl">Passion &  Sacrifice</h2>
         </div>
       </div>
     </main>
