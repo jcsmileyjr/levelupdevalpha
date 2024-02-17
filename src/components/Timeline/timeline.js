02b8fe1b32415c2c57a7c7f4c6b39242f111f292
JC <jcsmileyjr@gmail.com>
2024-02-17 15:56:18 -0600

    Styling:  Timeline component styling

--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -3,7 +3,7 @@ import postcss from "postcss";
 
 const Acheivement = ({content, position}) => {
     return (
-        <div className= {`min-w-64 w-64 min-h-24 p-8 rounded-full bg-red-200 flex flex-col items-center mb-8`} >
+        <div className= {`min-w-64 w-64 min-h-56 p-8 rounded-full bg-red-200 flex flex-col items-center mb-8`} >
             <h3 className="font-bold text-xl text-center">{content.title}</h3>
             <p className="m-2">{content.description}</p>
             <p className="font-medium">{content.date}</p>
@@ -16,20 +16,19 @@ const Timeline = ({data}) => {
         <section>
             <p className="text-center text-xl font-bold">Timeline</p>
             <div className="flex flex-row">
-                <div className="w-2/4 p-4">
+                <div className="w-2/4 p-4 border-r-2 border-dotted border-black flex flex-col items-center">
                     {
                         data.map( (entry, index) => {
-                            if(index % 2 !== 0) {
+                            if(index % 2 === 0) {
                                 return <Acheivement key={`entry-${index}`} content={entry} />
                             }
                         })
                     }                    
                 </div>
-                <div className="px-4">2</div>
-                <div className="w-2/4 p-4 mt-28">
+                <div className="w-2/4 p-4 mt-28 flex flex-col items-center">
                 {
                         data.map( (entry, index) => {
-                            if(index % 2 === 0) {
+                            if(index % 2 !== 0) {
                                 return <Acheivement key={`entry-${index}`} content={entry} />
                             }
                         })
