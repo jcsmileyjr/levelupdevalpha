9c1921ede7be84ad1f7049678b932b7e5f7ea0c4
JC <jcsmileyjr@gmail.com>
2024-02-17 19:39:49 -0600

    Feature: Landing page login and sign up buttons

--- src/app/page.js
+++ src/app/page.js
@@ -10,12 +10,16 @@ export default function Home() {
         <p className="text-base mt-2">Visually track your technical skills and career progression</p>
       </div>
       <div className="flex flex-col lg:flex-row ">
-        <div className="w-full order-last lg:order-first lg:w-2/3">
+        <div className="w-full order-last mt-6 lg:mt-0 lg:order-first lg:w-2/3">
           <h2 className="text-center text-2xl">Celebrate the Journey</h2>
           <Timeline data={AcheivementTestData} />
         </div>
         <div className="w-full lg:w-1/3 lg-6 lg:mt-0">
-          <h2 className="text-center text-2xl lg:text-xl">Passion &  Sacrifice</h2>
+          <div className='flex flex-col bg-babygreen rounded-lg p-12'>
+            <h2 className="text-center text-2xl lg:text-xl text-black font-bold ">Passion &  Sacrifice</h2>
+            <button className='p-2 mt-4 border border-primaryGreen font-bold text-2xl bg-white rounded-lg text-primaryGreen'>Log In</button>
+            <button className='p-2 mt-4 border border-primaryGreen font-bold text-2xl bg-white rounded-lg text-primaryGreen'>Sign Up</button>
+          </div>
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'babygreen': 'rgba(0, 175, 185, .05)',
+        'babyblue' :  'rgba(0, 95, 153, .05)',
+        'primaryGreen' : '#0081A7',
+      }
     },
   },
   plugins: [],
