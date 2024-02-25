5c0be3d14659f23ed1d9b0748d3e6751a6483394
JC <jcsmileyjr@gmail.com>
2024-02-24 20:38:00 -0600

    Feature: Initial header UI for Progress page

--- src/app/Progress/page.js
+++ src/app/Progress/page.js
@@ -1,10 +1,14 @@
-
+import Link from 'next/link';
 
 const Progress = () => {
     return (
-        <div>
-            Progress Page
-        </div>
+        <main className="flex min-h-screen flex-col p-4">
+            <section className="flex justify-between items-center">
+                {/*Landing page link */}
+                <Link className='font-bold underline text-primaryGreen text-base' href="/">Level Up Dev - Log-in</Link>
+                <Link className='font-bold text-gray-700 p-2 mt-0 border border-primaryGreen font-bold text-base bg-white rounded-lg' href="/">Sign out</Link>
+            </section>
+        </main>
     )
 }
 
