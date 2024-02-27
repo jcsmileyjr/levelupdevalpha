17725ff0675d30a323e4273ccd3a36de05383f66
JC <jcsmileyjr@gmail.com>
2024-02-27 17:20:30 -0600

    Feature: Sign up page UI minus error messages

--- src/app/SignUp/page.js
+++ src/app/SignUp/page.js
@@ -15,7 +15,16 @@ const SignUp = () => {
             </section>
             <div className="flex flex-col lg:flex-row mt-8 gap-6">
                 <form className="flex flex-col flex-1 w-full m-auto lg:m-0 sm:w-2/4 lg:w-1/3 lg-6 lg:mt-0 bg-babygreen rounded-lg min-h-64 h-2/3 p-4 lg:p-8 pb-8">
+                    {/* Email input field */}
+                    <label htmlFor='inputEmail' className='font-bold text-gray-700 text-2xl my-2'>Email address</label>
+                    <input id="inputEmail" name="inputEmail"  className="p-2 border border-primaryGreen bg-white text-xl rounded-lg" type="email"></input>
 
+                    {/* Name input field */}
+                    <label htmlFor='inputName' className='font-bold text-gray-700 text-2xl my-2'>First Name</label>
+                    <input id="inputName" name="inputName"  className="p-2 border border-primaryGreen bg-white text-xl rounded-lg" type="text"></input>
+
+                    {/*Submit button */}
+                    <button disabled={false} aria-label='Click to submit email and name to create an account.' type="submit" onClick={(e) => console.log("it works")} className="p-2 mt-6 border border-primaryGreen font-bold text-2xl bg-white rounded-lg text-primaryGreen" >Sign Up</button>                    
                 </form >
 
                 {/* Presentation Timeline Display */}
