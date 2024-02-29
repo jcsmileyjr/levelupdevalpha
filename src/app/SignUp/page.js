db9d6fd7d80f483cbda4768b3d031edc5f157bb5
JC <jcsmileyjr@gmail.com>
2024-02-28 20:32:34 -0600

    Feature Sign up page - initial submit functionality

--- src/app/SignUp/page.js
+++ src/app/SignUp/page.js
@@ -6,6 +6,16 @@ import Link from 'next/link';
 import LoginPromoData from '../../libs/dummyData/loginPromoData.json';
 
 const SignUp = () => {
+    const router = useRouter() //  Use to relocate user to another page
+
+    const [email, setEmail] = useState(""); // user entered email address
+    const [name, setName] = useState("") // user enter first name
+
+    const createUserProfile = (event) => {
+        event.preventDefault();
+        console.log(`User Profile: ${email} for ${name}`)
+    }
+
     return (
         <main className="flex min-h-screen flex-col p-4">
             <section className='flex flex-row justify-between'>
@@ -17,14 +27,14 @@ const SignUp = () => {
                 <form className="flex flex-col flex-1 w-full m-auto lg:m-0 sm:w-2/4 lg:w-1/3 lg-6 lg:mt-0 bg-babygreen rounded-lg min-h-64 h-2/3 p-4 lg:p-8 pb-8">
                     {/* Email input field */}
                     <label htmlFor='inputEmail' className='font-bold text-gray-700 text-2xl my-2'>Email address</label>
-                    <input id="inputEmail" name="inputEmail"  className="p-2 border border-primaryGreen bg-white text-xl rounded-lg" type="email"></input>
+                    <input id="inputEmail" onChange={(e) => setEmail(e.target.value)} name="inputEmail"  className="p-2 border border-primaryGreen bg-white text-xl rounded-lg" type="email"></input>
 
                     {/* Name input field */}
                     <label htmlFor='inputName' className='font-bold text-gray-700 text-2xl my-2'>First Name</label>
-                    <input id="inputName" name="inputName"  className="p-2 border border-primaryGreen bg-white text-xl rounded-lg" type="text"></input>
+                    <input id="inputName" onChange={(e) => setName(e.target.value)} name="inputName"  className="p-2 border border-primaryGreen bg-white text-xl rounded-lg" type="text"></input>
 
                     {/*Submit button */}
-                    <button disabled={false} aria-label='Click to submit email and name to create an account.' type="submit" onClick={(e) => console.log("it works")} className="p-2 mt-6 border border-primaryGreen font-bold text-2xl bg-white rounded-lg text-primaryGreen" >Sign Up</button>                    
+                    <button disabled={false} aria-label='Click to submit email and name to create an account.' type="submit" onClick={(e) => createUserProfile(e)} className="p-2 mt-6 border border-primaryGreen font-bold text-2xl bg-white rounded-lg text-primaryGreen" >Sign Up</button>                    
                 </form >
 
                 {/* Presentation Timeline Display */}
