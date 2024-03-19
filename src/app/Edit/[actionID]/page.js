9ff97d7ccd495b2ec357e1e2596a1cafa5578610
JC <jcsmileyjr@gmail.com>
2024-03-19 06:26:11 -0500

    Style for responsive design

--- src/app/Edit/[actionID]/page.js
+++ src/app/Edit/[actionID]/page.js
@@ -48,12 +48,12 @@ const Edit = ({params: {actionID}}) => {
                 </div>
                 <Link className='font-bold text-gray-700 p-2 mt-0 border border-primaryGreenDarker font-bold text-base bg-white rounded-lg min-w-24 flex justify-center items-center' href="/">Sign out</Link>
             </section>
-            <section>
+            <section className='flex flex-col sm:w-2/5 xl:w-1/4 sm:m-auto'>
                 <h1>Edit Item</h1>
 
                 {/* Update the Title*/}
                 <label className='text-xl text-gray-700 font-bold mb-2' htmlFor='inputTitle'>Title</label>
-                <div className='mb-6 w-full sm:w-2/4'>
+                <div className='mb-6 w-full '>
                     <input value={actionTitle} id="inputTitle" onChange={(e) => setActiontitle(e.target.value)} type="text" className='w-full p-2 border border-primaryGreen bg-white text-xl rounded-lg'></input>
                     <p className={`${actionTitleFormatError ? 'hidden':'block'} my-2 text-gray-700 text-base`}>20 character count limit</p>
                     <p className={`${actionTitleFormatError ? 'block' : 'hidden'} my-2 text-base text-red-700`}>Missing</p>
@@ -61,7 +61,7 @@ const Edit = ({params: {actionID}}) => {
 
                 {/* Update the reason */}
                 <label className='text-xl text-gray-700 font-bold mb-2' htmlFor='inputReason'>Reason</label>
-                <div className='mb-6 w-full sm:w-2/4'>
+                <div className='mb-6 w-full '>
                     <textarea value={actionDescription} id="inputReason" onChange={(e) => setActionDescription(e.target.value)} type="text" className='w-full p-2 border border-primaryGreen bg-white text-xl rounded-lg'></textarea>
                     {/*Error messages */}
                     <p className={`${actionDescriptionFormatError ? 'hidden':'block'} my-2 text-gray-700 text-base`}>40 character count limit</p>
@@ -71,7 +71,7 @@ const Edit = ({params: {actionID}}) => {
 
                 {/* Update month completed */}
                 <label className='text-xl text-gray-700 font-bold mb-2' htmlFor='selectMonth'>Month completed</label>
-                <div className='mb-6 w-full sm:w-2/4'>
+                <div className='mb-6 w-full '>
                     <select value={selectedMonth} onChange={(e) => setSelectedMonth(e.target.value)} name="selectMonth" id="selectMonth" type="text" className='w-full p-2 border border-primaryGreen bg-white text-xl rounded-lg'>
                         <option value="" key="default-month-option-1"></option>
                         {
@@ -85,13 +85,13 @@ const Edit = ({params: {actionID}}) => {
 
                 {/* Update year completed */}
                 <label className='text-xl text-gray-700 font-bold mb-2' htmlFor='selectedYear'>Year completed</label>
-                <div className='mb-6 w-full sm:w-2/4'>
+                <div className='mb-6 w-full '>
                     <input  maxLength={4} onChange={(e) => setSelectedYear(e.target.value)} type="text" value={selectedYear} id="selectedYear" name="selectedYear" className='w-full p-2 border border-primaryGreen bg-white text-xl rounded-lg'/>
                     <p className={`${selectedYearFormatError ? 'hidden':'block'} text-gray-700 text-base`}>Examples: 2007, 2014, 2023</p>
                     <p className={`${selectedYearFormatError ? 'block' : 'hidden'} mt-2 text-base text-red-700`}>Missing</p>
                 </div> 
 
-                <button type="submit" onClick={(e) => updateAction(e)} className='flex justify-center items-center p-2 mt-0 border border-primaryGreen font-bold text-xl bg-white rounded-lg text-primaryGreen w-1/2 sm:w-1/4 self-center'>
+                <button type="submit" onClick={(e) => updateAction(e)} className='flex justify-center items-center p-2 mt-0 border border-primaryGreen font-bold text-xl bg-white rounded-lg text-primaryGreen w-1/2 self-center'>
                     <Image priority={false} src={EditPen} width={20} height={20} alt="" className='mr-2' />
                     Update
                 </button>                                             
