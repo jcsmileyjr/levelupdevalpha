82ce9c4c6a317325739605c9dd1e2cfa7e609fe4
JC <jcsmileyjr@gmail.com>
2024-03-29 11:18:25 -0500

    Successful UI test cases for the Edit button

--- src/app/Edit/[type]/[actionID]/_tests_/edit.test.js
+++ src/app/Edit/[type]/[actionID]/_tests_/edit.test.js
@@ -12,4 +12,28 @@ describe('Edit page', () =>  {
         const header = screen.getByRole("heading", {name: "Edit Item"});
         expect(header).toBeInTheDocument()
     })
+
+    it('should render both input fields Reason', () => {
+        render(<Edit params={{type: 'Projects', actionID : "5"}} />)
+        const inputReason = screen.getByRole("textbox", {name: 'Reason'});
+        expect(inputReason).toBeInTheDocument();        
+    })
+
+    it('should render select field for Month Completed', () => {
+        render(<Edit params={{type: 'Projects', actionID : "5"}} />)
+        const monthSelected = screen.getByRole("combobox", {name: 'Month completed'});
+        expect(monthSelected).toBeInTheDocument();        
+    })
+
+    it('should render input field to Year completed', () => {
+        render(<Edit params={{type: 'Projects', actionID : "5"}} />)
+        const input = screen.getByRole("textbox", {name: 'Year completed'});
+        expect(input).toBeInTheDocument();      
+    })
+
+    it('should render button for Update', () => {    
+        render(<Edit params={{type: 'Projects', actionID : "5"}} />)
+        const button = screen.getByRole("button", {name: "Update"});
+        expect(button).toBeInTheDocument(); 
+    })
 })
