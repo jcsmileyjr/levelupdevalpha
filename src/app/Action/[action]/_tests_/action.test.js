f6b4e81465cd7ada9d653d513c2888fc9a7e2195
JC <jcsmileyjr@gmail.com>
2024-04-02 11:38:02 -0500

    Update Edit testing to check getting and updating data

--- src/app/Action/[action]/_tests_/action.test.js
+++ src/app/Action/[action]/_tests_/action.test.js
@@ -1,5 +1,5 @@
 import {getByRole ,render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event';
 import Action from '../page';
 
 describe('Action page', () =>  {
